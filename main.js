f91f6ea3cb6943c4e47753befea37669744f2006
arnav10000 <83585771+arnav10000@users.noreply.github.com>
2021-06-13 15:39:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(640, 480);
-    canvas.position(110,  250);
+    canvas.center
     video = createCapture(VIDEO);
     video.hide();
 
